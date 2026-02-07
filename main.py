945485678ae6ac70fb7656d52227fc0a21122e4c
gopal-nd <docode999@gmail.com>
2026-02-07 15:43:59 +0530

    init

---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+data = {
+    'name':["gopal","Reddy","ND","superman"],
+    'age':[20,23,99,None],
+    'work':['remort','in person','user man','stap']
+}
+df = pd.DateFrame(data)
+df
